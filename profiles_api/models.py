a433f586ef1ae1f4dd0a4b3dac34e4077e354e76
Awabkhaled <khaledawab3@gmail.com>
2024-03-30 23:57:43 +0200

    Added profiles_api app, created the user and manager for it and feed items and handled authentications for it, added the admin config

---
+++ profiles_api/models.py
@@ -0,0 +1,53 @@
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.models import PermissionsMixin 
+from django.db import models 
+from django.conf import settings
+
+class UserManager(BaseUserManager):
+    
+    def create_user(self,email,name,password=None):
+        if not email: raise ValueError('You must enter email')  
+        email = self.normaliza_email(email) 
+        user = self.model(email=email,name=name)  
+        user.set_password(password) 
+        self.save(using=self._db)  
+        return user 
+    def create_superuser(self,email,name,password):
+        user  = self.create_user(email,name,password)  
+        user.is_superuser=True
+        user.staff=True
+        user.save(using=self._db)  
+        return user
+        
+    
+class UserProfile(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(max_length=25,unique=True)  
+    name = models.CharField(max_length=25)
+    password = models.CharField(max_length=100)
+    is_active = models.BooleanField(default = True)
+    is_staff = models.BooleanField(default = False)
+    
+    objects = UserManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+    
+    def get_full_name(self):
+        return self.name
+    def get_short_name(self):
+        return self.name
+    def __str__(self):
+        return self.name+": "+self.email
+
+
+class ProfileFeedItem(models.Model):
+    user_profile = models.ForeignKey(  
+        settings.AUTH_USER_MODEL,  
+        on_delete = models.CASCADE,
+    )
+    creation_time = models.DateTimeField(auto_now_add = True) 
+    status = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.status+", created at "+self.creation_time
