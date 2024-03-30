d4b82a682287ec6994ba3810592d723de2cf16aa
Awabkhaled <khaledawab3@gmail.com>
2024-03-31 00:08:45 +0200

    Fixed some errors in the model file

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -8,19 +8,19 @@ class UserManager(BaseUserManager):
     
     def create_user(self,email,name,password=None):
         if not email: raise ValueError('You must enter email')  
-        email = self.normaliza_email(email) 
+        email = self.normalize_email(email) 
         user = self.model(email=email,name=name)  
         user.set_password(password) 
-        self.save(using=self._db)  
+        user.save(using=self._db)  
         return user 
     def create_superuser(self,email,name,password):
         user  = self.create_user(email,name,password)  
         user.is_superuser=True
-        user.staff=True
+        user.is_staff=True
         user.save(using=self._db)  
         return user
-        
-    
+
+
 class UserProfile(AbstractBaseUser,PermissionsMixin):
     email = models.EmailField(max_length=25,unique=True)  
     name = models.CharField(max_length=25)
